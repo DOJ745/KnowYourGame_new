50fa3ccdee4a28bf618cd045f56c9191b8fb743d
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-04-29 09:19:26 +0300

    Dialog fragment works fine

--- app/src/main/java/com/faa/knowyourgame_new/MainActivity.java
+++ app/src/main/java/com/faa/knowyourgame_new/MainActivity.java
@@ -23,6 +23,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -35,7 +36,7 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
     private ServerService myService;
-    //private Activity forDialog = new Activity();
+    private DialogFragment loginDialogFragment= new TestLogin();
     private static final String TAG = "MainActivity";
 
     //private User test_user = new User();
@@ -67,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
 
         NavigationUI.setupWithNavController(navView, navController);
 
-        showLoginDialog();
+        loginDialogFragment.show(getSupportFragmentManager(), "TEST_DIALOG");
 
 
         /*binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
@@ -90,83 +91,6 @@ public class MainActivity extends AppCompatActivity {
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo netInfo = cm.getActiveNetworkInfo();
         return netInfo != null && netInfo.isConnectedOrConnecting();
-    }
-
-    private void showLoginDialog() {
-
-        //TestLogin loginDialog = new TestLogin();
-        //loginDialog.createDialog(this, ).show();
-        /*
-        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-        dialog.setCancelable(false);
-
-        View view = getLayoutInflater().inflate(R.layout.login_dialog, null);
-        dialog.setView(view);
-
-
-        EditText entered_login = view.findViewById(R.id.editLogin);
-        EditText entered_password = view.findViewById(R.id.editPassword);
-
-        Button sign_in = view.findViewById(R.id.button_sign_in);
-        Button sign_up = view.findViewById(R.id.button_sign_up);
-
-        Dialog loginDialog = dialog.create();
-
-        sign_in.setOnClickListener( listener_in -> {
-
-            if(hasConnection(this)) {
-                Toast.makeText(this, "sign in", Toast.LENGTH_SHORT).show();
-                loginDialog.cancel();
-            }
-            else
-                Toast.makeText(this, "no internet connection", Toast.LENGTH_SHORT).show();
-        });
-
-        sign_up.setOnClickListener( listener_up -> {
-            if(hasConnection(this)) {
-                Toast.makeText(this, "sign up", Toast.LENGTH_SHORT).show();
-                loginDialog.cancel();
-            }
-            else
-                Toast.makeText(this, "no internet connection", Toast.LENGTH_SHORT).show();
-        });
-
-        entered_login.setError("Required field! (> 3 symbols!)");
-        entered_login.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
-            @Override
-            public void afterTextChanged(Editable editable) {
-                if (entered_login.getText().length() != 0 && entered_login.getText().length() > 3) {
-                    entered_login.setError(null);
-                }
-                else {
-                    entered_login.setError("Required!");
-                }
-            }
-        });
-
-        entered_password.setError("Required field! (> 5 symbols!)");
-        entered_password.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
-            @Override
-            public void afterTextChanged(Editable editable) {
-                if (entered_password.getText().length() != 0 && entered_password.getText().length() > 5) {
-                    entered_password.setError(null);
-                }
-                else {
-                    entered_password.setError("Required!");
-                }
-            }
-        });
-        loginDialog.show();*/
     }
 
     public void signIn() {
--- app/src/main/java/com/faa/knowyourgame_new/ui/login_dialog/TestLogin.java
+++ app/src/main/java/com/faa/knowyourgame_new/ui/login_dialog/TestLogin.java
@@ -16,18 +16,9 @@ import androidx.fragment.app.DialogFragment;
 
 import com.faa.knowyourgame_new.R;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import static com.faa.knowyourgame_new.MainActivity.hasConnection;
 
-@Getter
-@Setter
 public class TestLogin extends DialogFragment implements DialogInterface.OnClickListener {
-
-    //private Dialog loginDialog;
-    //private Context activityContext;
-
 
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
@@ -46,8 +37,7 @@ public class TestLogin extends DialogFragment implements DialogInterface.OnClick
 
         Dialog loginDialog = dialog.create();
 
-        sign_in.setOnClickListener( listener_in -> {
-
+        sign_in.setOnClickListener(listener_in -> {
             if(hasConnection(getActivity())) {
                 Toast.makeText(getActivity(), "sign in", Toast.LENGTH_SHORT).show();
                 loginDialog.cancel();
@@ -56,7 +46,7 @@ public class TestLogin extends DialogFragment implements DialogInterface.OnClick
                 Toast.makeText(getActivity(), "no internet connection", Toast.LENGTH_SHORT).show();
         });
 
-        sign_up.setOnClickListener( listener_up -> {
+        sign_up.setOnClickListener(listener_up -> {
             if(hasConnection(getActivity())) {
                 Toast.makeText(getActivity(), "sign up", Toast.LENGTH_SHORT).show();
                 loginDialog.cancel();
@@ -103,95 +93,7 @@ public class TestLogin extends DialogFragment implements DialogInterface.OnClick
 
         return loginDialog;
     }
-    /*
-    public Dialog createDialog(Context context) {
-
-        AlertDialog.Builder dialog = new AlertDialog.Builder(context);
-        dialog.setCancelable(false);
-
-        View view = activity.getLayoutInflater().inflate(R.layout.login_dialog, null);
-        dialog.setView(view);
-
-
-        EditText entered_login = view.findViewById(R.id.editLogin);
-        EditText entered_password = view.findViewById(R.id.editPassword);
-
-        Button sign_in = view.findViewById(R.id.button_sign_in);
-        Button sign_up = view.findViewById(R.id.button_sign_up);
-
-        Dialog loginDialog = dialog.create();
-
-        sign_in.setOnClickListener( listener_in -> {
-
-            if(hasConnection(context)) {
-                Toast.makeText(context, "sign in", Toast.LENGTH_SHORT).show();
-                loginDialog.cancel();
-            }
-            else
-                Toast.makeText(context, "no internet connection", Toast.LENGTH_SHORT).show();
-        });
-
-        sign_up.setOnClickListener( listener_up -> {
-            if(hasConnection(context)) {
-                Toast.makeText(context, "sign up", Toast.LENGTH_SHORT).show();
-                loginDialog.cancel();
-            }
-            else
-                Toast.makeText(context, "no internet connection", Toast.LENGTH_SHORT).show();
-        });
-
-        entered_login.setError("Required field! (> 3 symbols!)");
-        entered_login.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
-            @Override
-            public void afterTextChanged(Editable editable) {
-                if (entered_login.getText().length() != 0 && entered_login.getText().length() > 3) {
-                    entered_login.setError(null);
-                }
-                else {
-                    entered_login.setError("Required!");
-                }
-            }
-        });
-
-        entered_password.setError("Required field! (> 5 symbols!)");
-        entered_password.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
-            @Override
-            public void afterTextChanged(Editable editable) {
-                if (entered_password.getText().length() != 0 && entered_password.getText().length() > 5) {
-                    entered_password.setError(null);
-                }
-                else {
-                    entered_password.setError("Required!");
-                }
-            }
-        });
-
-        return loginDialog;
-    }*/
 
     @Override
-    public void onClick(DialogInterface dialog, int which) {
-
-        switch (which) {
-            case R.id.button_sign_in:
-                if(hasConnection(getActivity())) {
-                    Toast.makeText(getActivity(), "sign in", Toast.LENGTH_SHORT).show();
-                    super.onCancel(dialog);
-                }
-                else
-                    Toast.makeText(getActivity(), "no internet connection", Toast.LENGTH_SHORT).show();
-                break;
-        }
-
-    }
+    public void onClick(DialogInterface dialog, int which) { }
 }
