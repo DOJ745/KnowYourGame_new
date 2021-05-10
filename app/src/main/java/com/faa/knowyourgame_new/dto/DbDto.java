7060dca65a4dc036b19018d258d33e3afb60d313
DOJ745 <andreyfa12@mail.ru>
2021-05-10 21:24:00 +0300

    Changed List to ArrayList

--- app/src/main/java/com/faa/knowyourgame_new/dto/DbDto.java
+++ app/src/main/java/com/faa/knowyourgame_new/dto/DbDto.java
@@ -1,15 +1,15 @@
 package com.faa.knowyourgame_new.dto;
 
 import java.io.Serializable;
-import java.util.List;
+import java.util.ArrayList;
 
 import lombok.Data;
 
 @Data
 public class DbDto implements Serializable {
-    List<AnswersDto> answers;
-    List<LeagueDto> leagues;
-    List<ThemeDto> themes;
-    List<DifficultyDto> difficulties;
-    List<QuestionDto> questions;
+    ArrayList<AnswersDto> answers;
+    ArrayList<LeagueDto> leagues;
+    ArrayList<ThemeDto> themes;
+    ArrayList<DifficultyDto> difficulties;
+    ArrayList<QuestionDto> questions;
 }
